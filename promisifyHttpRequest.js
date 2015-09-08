6ea99895920e3bc4d298ccefd7c3799a61f0a3c1
Elankeeran <ekeeran@gmail.com>
2015-09-08 10:29:28 +0530

    adding promisify http request code

---
+++ promisifyHttpRequest.js
@@ -0,0 +1,52 @@
+var http = require('http');
+var Promise = require('bluebird');
+
+var PromiseRequest = Promise.method(function(options) {
+    return new Promise(function(resolve, reject) { 
+        var request = http.request(options, function(response) {
+            // Bundle the result
+            var result = {
+                'httpVersion': response.httpVersion,
+                'httpStatusCode': response.statusCode,
+                'headers': response.headers,
+                'body': '',
+                'trailers': response.trailers,
+            };
+
+            // Build the body
+            response.on('data', function(chunk) {
+                result.body += chunk;
+            });
+
+            // Resolve the promise
+            //resolve(result);
+
+    		// Resolve the promise when the response ends
+    		response.on('end', function() {
+        			resolve(result);
+    		});
+        });
+
+        // Handle errors
+        request.on('error', function(error) {
+            console.log('Problem with request:', error.message);
+            reject(error);
+        });
+
+        // Must always call .end() even if there is no data being written to the request body
+        request.end();
+    });
+});
+
+export.promisifyHttpRequest = PromiseRequest;
+
+/*
+var myRequest = PromiseRequest({
+    method: 'GET',
+    host: 'ela.com',
+    port: 80,
+    path: '/weatherReports/1000',
+}).then(function(value) {
+    console.log('value:', value);
+});
+*/
